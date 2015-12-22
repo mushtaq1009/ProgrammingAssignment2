ed349a657e136b05ebd4b67032ecbc41ecc1c3f9
Mushtaq Mohammed <mushtaqdba@gmail.com>
2015-12-22 00:27:12 -0500

    added comments for the function as per the instructions

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## This function will do the inverse of the matrix by utilizing lexical scoping 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
